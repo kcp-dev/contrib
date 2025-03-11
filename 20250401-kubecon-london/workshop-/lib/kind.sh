8583178f7a24f40c646e50776b2d0e9821369bab
Robert Vasek <rvasek01@gmail.com>
2025-03-11 09:31:34 +0100

    moved kind cluster creation/deletion to its own fn

--- 20250401-kubecon-london/workshop-/lib/kind.sh
+++ 20250401-kubecon-london/workshop-/lib/kind.sh
@@ -9,9 +9,43 @@ export KREW_ROOT="${workshop_root}/bin/.krew"
 export PATH="${workshop_root}/bin/.krew/bin:${workshop_root}/bin:${PATH}"
 export KUBECONFIG="${workshop_root}/.kcp/admin.kubeconfig"
 
-function ::kind::cluster::create {
+kind_service_name='workshop-kcp-kind.scope'
+
+function ::kind::create::cluster {
   name="${1}"
+  kubeconfig_path="${2}"
   printf "\n✨Creating a kind cluster '${name}':\n"
-  printf "\$ kind cluster create ${name}\n"
-  kind cluster create "${name}"
+  printf "\$ kind create cluster ${name}\n"
+
+  create_cmd="kind create cluster --name ${name} --kubeconfig ${kubeconfig_path}"
+
+  if [ "$(uname -s)" = "Linux" ]; then
+    # Assuming we're on a systemd system.
+    # In certain environments (e.g. Fedora with podman), there may be
+    # issues with running kind in rootless mode. Containing it in a
+    # separate service with cgroups delegated to the parent process
+    # works around that issue.
+    # See https://kind.sigs.k8s.io/docs/user/rootless/
+    # and https://lists.fedoraproject.org/archives/list/devel@lists.fedoraproject.org/thread/ZMKLS7SHMRJLJ57NZCYPBAQ3UOYULV65/
+    systemd-run --user --scope \
+      --unit="${kind_service_name}" \
+      --property=Delegate=yes \
+      ${create_cmd}
+  else
+    ${create_cmd}
+  fi
+}
+
+function ::kind::delete::cluster {
+  name="${1}"
+  printf "\n✨Deleting kind cluster '${name}':\n"
+  printf "\$ kind delete cluster --name ${name}\n"
+
+  kind delete cluster --name "${name}" || true
+
+  if [ "$(uname -s)" = "Linux" ]; then
+    # Clean the scope created in ::kind::create::cluster.
+    systemctl --user stop "${kind_service_name}"
+    systemctl --user reset-failed "${kind_service_name}"
+  fi
 }
