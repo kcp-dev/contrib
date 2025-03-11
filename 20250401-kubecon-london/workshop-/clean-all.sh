d6a816933454b70f7cc7e16dd27e5cd12d15d370
Robert Vasek <rvasek01@gmail.com>
2025-03-11 10:10:18 +0100

    added clean-all script

---
+++ 20250401-kubecon-london/workshop-/clean-all.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+export workshop_root="$(git rev-parse --show-toplevel)/20250401-kubecon-london/workshop-"
+export PATH="${workshop_root}/bin:${PATH}"
+export KUBECONFIGS_DIR="${workshop_root}/kubeconfigs"
+source "${workshop_root}/lib/kind.sh"
+
+set +o errexit
+
+pkill api-syncagent && echo "🥷 stopped api-syncagent"
+pkill kcp && echo "🥷 stopped kcp"
+
+kind_cluster_name='provider'
+::kind::delete::cluster "${kind_cluster_name}"
+
+rm -rf "${KUBECONFIGS_DIR}"
+rm ${workshop_root}/.checkpoint-*
