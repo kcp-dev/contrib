a6adf8d3bf36b1f6afafe0d5fe6c685faeb07644
Robert Vasek <rvasek01@gmail.com>
2025-03-19 16:19:18 +0100

    04-application-providers: moved creating consumers step

--- 20250401-kubecon-london/workshop/04-application-providers/02-consumer.sh
+++ 20250401-kubecon-london/workshop/04-application-providers/02-consumer.sh
@@ -10,16 +10,15 @@ export PATH="${workshop_root}/bin/.krew/bin:${workshop_root}/bin:${PATH}"
 export KUBECONFIGS_DIR="${workshop_root}/kubeconfigs"
 export exercise_dir="$(dirname "${BASH_SOURCE[0]}")"
 source "${workshop_root}/lib/kubectl.sh"
-source "${workshop_root}/lib/api-syncagent.sh"
 
-# [[ -f "${workshop_root}/.checkpoint-02" ]] || { printf "\n\t📜 You need to complete the previous exercise!\n\n" ; exit 1 ; }
+[[ -f "${workshop_root}/.checkpoint-03" ]] || { printf "\n\t📜 You need to complete the previous exercise!\n\n" ; exit 1 ; }
 
 export KUBECONFIG="${KUBECONFIGS_DIR}/admin.kubeconfig"
 
 ::kubectl::ws::use ":root:consumers:pg"
 # TODO: add flag to accept secrets
-::kubectl::kcp::bind_apiexport "root:providers:application" "apis.contrib.kcp.io" "apis.contrib.kcp.io"
-
+# ::kubectl::kcp::bind_apiexport "root:providers:application" "apis.contrib.kcp.io" "apis.contrib.kcp.io"
+::kubectl::kcp::bind_with_permission_claims "root:providers:application" "apis.contrib.kcp.io" "apis.contrib.kcp.io" "secrets.core" ""
 ::kubectl::create_from_file "${exercise_dir}/apis/application.yaml"
 
 # TODO(Add how to port forward from the application)
